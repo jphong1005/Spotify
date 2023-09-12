b0139f11481fb3a5bc8f1157af9fcf4654ad7e73
JP <wlsvy1005@gmail.com>
2023-09-12 21:30:14 +0900

    [Refactor]: APICaller - 각 메서드의 중복되어 실행되는 .withValidToken(completionHandler: @escaping (String) -> Void)를 따로 분리하면서 그 안에 HTTP Networking 및  Publisher에게 추상화 값을 전달하는 방식으로 재구성

--- Spotify/Spotify/Managers/APICaller.swift
+++ Spotify/Spotify/Managers/APICaller.swift
@@ -22,66 +22,44 @@ final class APICaller {
     private init() {}
     
     // MARK: - Methods
-    public func getCurrentUserProfile() -> Observable<UserProfile> {
+    //  PRIVATE Helper Method
+    private func performRequest<T: Codable>(query: String, method: HTTPMethod, params: Parameters? = nil, headers: HTTPHeaders? = nil) -> Observable<T> {
         
-        return Observable.create { observer in
+        return Observable.create { observser in
             AuthManager.shared.withValidToken { token in
                 let headers: HTTPHeaders = HTTPHeaders([
                     "Authorization": "Bearer \(token)"
                 ])
                 
-                AF.request(APICaller.defaultEndPoint + "/me",
+                AF.request(APICaller.defaultEndPoint + query,
                            method: .get,
                            headers: headers)
                 .validate(statusCode: 200 ..< 300)
                 .validate(contentType: ["application/json"])
-                .responseDecodable(of: UserProfile.self, queue: DispatchQueue.global(qos: .background)) { response in
+                .responseDecodable(of: T.self,
+                                   queue: DispatchQueue.global(qos: .background)) { response in
                     switch response.result {
-                    case .success(let userProfile):
-                        dump("\(userProfile) \n")
-                        observer.onNext(userProfile)
-                        observer.onCompleted(); break;
+                    case .success(let data):
+                        observser.onNext(data)
+                        observser.onCompleted(); break;
                     case .failure(let error):
-                        print("error: \(error.localizedDescription)")
-                        observer.onError(error); break;
+                        observser.onError(error); break;
                     }
                 }
             }
             
             return Disposables.create()
-        }.asObservable()
+        }
+    }
+    
+    //  PUBLIC API Methods
+    public func getCurrentUserProfile() -> Observable<UserProfile> {
+        
+        return performRequest(query: "/me", method: .get)
     }
     
     public func getNewReleases() -> Observable<NewReleasesResponse> {
         
-        return Observable.create { observer in
-            AuthManager.shared.withValidToken { token in
-                let headers: HTTPHeaders = HTTPHeaders([
-                    "Authorization": "Bearer \(token)"
-                ])
-                
-                print(token)
-                print("?: \(headers)")
-                
-                AF.request(APICaller.defaultEndPoint + "/browse/new-releases?limit=50",
-                           method: .get,
-                           headers: headers)
-                .validate(statusCode: 200 ..< 300)
-                .validate(contentType: ["application/json"])
-                .responseDecodable(of: NewReleasesResponse.self, queue: DispatchQueue.global(qos: .background)) { response in
-                    switch response.result {
-                    case .success(let newRelease):
-                            dump("\(newRelease) \n")
-                            observer.onNext(newRelease)
-                            observer.onCompleted(); break;
-                    case .failure(let error):
-                        print("error: \(error.localizedDescription)")
-                        observer.onError(error); break;
-                    }
-                }
-            }
-            
-            return Disposables.create()
-        }.asObservable()
+        return performRequest(query: "/browse/new-releases?limit=50", method: .get)
     }
 }
